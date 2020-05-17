10ea8665cf48001fcba3153c3d40d6cb0b584e91
DinhKhangHoang <hdkhang1504@gmail.com>
2020-05-17 21:48:29 +0700

    init Account-user profile

--- client/src/pages/Account.js
+++ client/src/pages/Account.js
@@ -1,5 +1,52 @@
 import React from "react";
+import SideBar from "../components/SideBar/SideBar";
+import AccountCircleIcon from '@material-ui/icons/AccountCircle';
+import EqualizerIcon from '@material-ui/icons/Equalizer';
+import ToysIcon from '@material-ui/icons/Toys';
+import {Route} from 'react-router-dom';
+import UserInfo from '../components/UserInfo/UserInfo';
+import style from './Account.module.css';
 
-export const Account = () => {
-  return <div>Account Page</div>;
+const items = [
+  {name: "userInformation", label: "User Info", Icon: AccountCircleIcon, url: "/account/info"},
+  "divider",
+  {
+    name: "statistic",
+    label: "Activity Logs",
+    Icon: EqualizerIcon,
+    items: [
+      {
+        name: "generalLogs",
+        label: "General Logs",
+        
+      },
+      {
+        name: "detailedLogs",
+        label: "Detailed Logs",
+        
+      }
+    ]
+  },
+  "divider",
+  {
+    name: "manageDevices",
+    label: "Manage Devices",
+    Icon: ToysIcon,
+    
+  }
+];
+
+export const Account = (props) => {
+  console.log(props);
+  return (
+    <div className={style.Account}>
+      <div className={style.SideBar}>
+        <SideBar items = {items} {...props}/>
+      </div>
+      <div className={style.Page}>
+        <Route path="/account/info" component={UserInfo}/>
+      </div>
+      
+    </div>
+    );
 };
