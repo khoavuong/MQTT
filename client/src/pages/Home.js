573951a14f546095a0be562a105967fcfb071bb4
khoavuong <50255410+khoavuong@users.noreply.github.com>
2020-06-06 14:28:02 +0700

    add MQTT's interaction

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,6 +1,7 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
-import { Tabs, Collapse, InputNumber } from "antd";
+import { Tabs, Collapse, InputNumber, Switch } from "antd";
+import mqtt from "mqtt";
 
 const { TabPane } = Tabs;
 const { Panel } = Collapse;
@@ -19,15 +20,72 @@ const ContentDisplay = styled.div`
   align-items: center;
 `;
 
+const options = {
+  protocol: "mqtts",
+};
+const temperatureConnection = mqtt.connect(
+  "mqtt://test.mosquitto.org:8081",
+  options
+);
+const humidityConnection = mqtt.connect(
+  "mqtt://test.mosquitto.org:8081",
+  options
+);
+const fanConnection = mqtt.connect("mqtt://test.mosquitto.org:8081", options);
+const ACConnection = mqtt.connect("mqtt://test.mosquitto.org:8081", options);
+
+temperatureConnection.subscribe("temperature-bku");
+humidityConnection.subscribe("humidity-bku");
+
 export const Home = () => {
+  const [temperature, setTemperature] = useState(
+    localStorage.getItem("temperature")
+      ? localStorage.getItem("temperature")
+      : "Loading"
+  );
+  const [humidity, setHumidity] = useState(
+    localStorage.getItem("humidity")
+      ? localStorage.getItem("humidity")
+      : "Loading"
+  );
+  // const [fan, setFan] = useState("Loading");
+
+  temperatureConnection.on("message", function (topic, message) {
+    setTemperature(message.toString());
+    localStorage.setItem("temperature", message.toString());
+  });
+
+  humidityConnection.on("message", function (topic, message) {
+    setHumidity(message.toString());
+    localStorage.setItem("humidity", message.toString());
+  });
+
+  function acChange(checked) {
+    localStorage.setItem("ac", checked);
+    ACConnection.publish("ac-bku", checked ? "On" : "Off");
+  }
+
+  function fanChange(checked) {
+    localStorage.setItem("fan", checked);
+    fanConnection.publish("fan-bku", checked ? "On" : "Off");
+  }
+
   return (
     <Container>
       <Tabs>
         <TabPane tab="Nhiệt độ / Độ ẩm" key="1">
           <Collapse>
             <Panel header="Phòng khách" key="1">
-              <p>Nhiệt độ: 30</p>
-              <p>Độ ẩm: 20</p>
+              <p>
+                Nhiệt độ:{" "}
+                <b style={{ color: temperature >= 30 ? "red" : "" }}>
+                  {temperature}
+                </b>
+              </p>
+              <p>
+                Độ ẩm:{" "}
+                <b style={{ color: humidity >= 30 ? "red" : "" }}>{humidity}</b>
+              </p>
             </Panel>
             <Panel header="Phòng ngủ" key="2">
               <p>Nhiệt độ: 30</p>
@@ -39,7 +97,9 @@ export const Home = () => {
           <Collapse>
             <Panel header="Phòng khách" key="1">
               <ContentDisplay style={{ marginBottom: "30px" }}>
-                <div>Máy điều hòa 1:</div>
+                <div>
+                  <b>Máy điều hòa:</b>
+                </div>
                 <ContentDisplay>
                   <div style={{ marginRight: "5px" }}>Nhiệt độ: </div>
                   <InputNumber min={1} max={100} defaultValue={25} />
@@ -48,41 +108,28 @@ export const Home = () => {
                   <div style={{ marginRight: "5px" }}>Độ ẩm: </div>
                   <InputNumber min={1} max={100} defaultValue={30} />
                 </ContentDisplay>
+                <Switch
+                  defaultChecked={
+                    localStorage.getItem("ac") === "true" ? true : false
+                  }
+                  onChange={acChange}
+                ></Switch>
               </ContentDisplay>
 
               <ContentDisplay style={{ marginBottom: "30px" }}>
-                <div>Máy điều hòa 2:</div>
-                <ContentDisplay>
-                  <div style={{ marginRight: "5px" }}>Nhiệt độ: </div>
-                  <InputNumber min={1} max={100} defaultValue={25} />
-                </ContentDisplay>
-                <ContentDisplay>
-                  <div style={{ marginRight: "5px" }}>Độ ẩm: </div>
-                  <InputNumber min={1} max={100} defaultValue={30} />
-                </ContentDisplay>
-              </ContentDisplay>
-
-              <ContentDisplay style={{ marginBottom: "30px" }}>
-                <div>Quạt 1:</div>
-                <ContentDisplay>
-                  <div style={{ marginRight: "5px" }}>Sức gió: </div>
-                  <InputNumber min={1} max={100} defaultValue={25} />
-                </ContentDisplay>
-              </ContentDisplay>
-
-              <ContentDisplay>
-                <div>Quạt 2:</div>
-                <ContentDisplay>
-                  <div style={{ marginRight: "5px" }}>Sức gió: </div>
-                  <InputNumber min={1} max={100} defaultValue={25} />
-                </ContentDisplay>
+                <div>
+                  <b>Quạt:</b>
+                </div>
+                <Switch
+                  defaultChecked={
+                    localStorage.getItem("fan") === "true" ? true : false
+                  }
+                  onChange={fanChange}
+                ></Switch>
               </ContentDisplay>
             </Panel>
             <Panel header="Phòng ngủ" key="2"></Panel>
           </Collapse>
-        </TabPane>
-        <TabPane tab="Cài đặt cá nhân" key="3">
-          Content
         </TabPane>
       </Tabs>
     </Container>
