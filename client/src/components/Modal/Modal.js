7368fc2ea1341d042b775d779d7013781c466306
khoavuong <50255410+khoavuong@users.noreply.github.com>
2020-06-14 11:41:50 +0700

    Aux -> Aus

--- client/src/components/Modal/Modal.js
+++ client/src/components/Modal/Modal.js
@@ -1,22 +1,28 @@
-import React from 'react';
-import styles from './Modal.module.css';
-import Aux from '../../hoc/Aux';
-import Backdrop from '../Backdrop/Backdrop';
+import React from "react";
+import styles from "./Modal.module.css";
+import Aus from "../../hoc/Aus";
+import Backdrop from "../Backdrop/Backdrop";
 
-const modal=(props)=>{
-    return(<Aux>
-        <Backdrop show={props.show} clicked={props.modalClosed}/>
-        <div className={styles.Modal}
+const modal = (props) => {
+  return (
+    <Aus>
+      <Backdrop show={props.show} clicked={props.modalClosed} />
+      <div
+        className={styles.Modal}
         style={{
-            transform: props.show ? 'translateY(0)': 'translateY(-100vh)',
-            opacity: props.show ? '1': '0'
-            }}>
-            {props.children}
-        </div>
-    </Aux>
-    
-);}
-const areEqual=(prevProps, nextProps)=>{
-    return prevProps.show === nextProps.show && prevProps.children === nextProps.children;
-}
+          transform: props.show ? "translateY(0)" : "translateY(-100vh)",
+          opacity: props.show ? "1" : "0",
+        }}
+      >
+        {props.children}
+      </div>
+    </Aus>
+  );
+};
+const areEqual = (prevProps, nextProps) => {
+  return (
+    prevProps.show === nextProps.show &&
+    prevProps.children === nextProps.children
+  );
+};
 export default React.memo(modal, areEqual);
--- client/src/components/UserInfo/UserInfo.js
+++ client/src/components/UserInfo/UserInfo.js
@@ -1,133 +1,123 @@
-import React, {useState} from 'react';
-import Card from '@material-ui/core/Card';
-import CardMedia from '@material-ui/core/CardMedia';
-import CardContent from '@material-ui/core/CardContent';
-import {makeStyles} from '@material-ui/core/styles';
-import Typography from '@material-ui/core/Typography';
-import { red } from '@material-ui/core/colors';
-import userimage from '../../assets/user.png';
-import Button from '@material-ui/core/Button';
-import Aux from '../../hoc/Aux';
-import Modal from '../Modal/Modal';
-import SetPass from '../SetPass/SetPass';
-import AddUser from '../AddUser/AddUser';
+import React, { useState } from "react";
+import Card from "@material-ui/core/Card";
+import CardMedia from "@material-ui/core/CardMedia";
+import CardContent from "@material-ui/core/CardContent";
+import { makeStyles } from "@material-ui/core/styles";
+import Typography from "@material-ui/core/Typography";
+import { red } from "@material-ui/core/colors";
+import userimage from "../../assets/user.png";
+import Button from "@material-ui/core/Button";
+import Aus from "../../hoc/Aus";
+import Modal from "../Modal/Modal";
+import SetPass from "../SetPass/SetPass";
+import AddUser from "../AddUser/AddUser";
 
 const useStyles = makeStyles((theme) => ({
-    root: {
-      maxWidth: '80%',
-      margin: 'auto',
-      marginTop: '50px',
-      height: '70vh',
-      display: 'flex',
-      flexDirection: 'column',
-      alignItems: 'center',
-      borderWidth: '5px',
-      zIndex: '50',
-      borderColor: 'black'
-    },
-    media: {
-      height: '100px',
-      paddingTop: '10.25%', // 16:9
-      width: '100px',
-      borderRadius: '50%'
-    },
-    avatar: {
-      backgroundColor: red[500],
-    },
-    content:{
-      width: '50%'
-    },
-    field:{
-      display: 'flex',
-      justifyContent: 'space-between',
-      paddingTop: '10px'
-    }
-  }));
+  root: {
+    maxWidth: "80%",
+    margin: "auto",
+    marginTop: "50px",
+    height: "70vh",
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    borderWidth: "5px",
+    zIndex: "50",
+    borderColor: "black",
+  },
+  media: {
+    height: "100px",
+    paddingTop: "10.25%", // 16:9
+    width: "100px",
+    borderRadius: "50%",
+  },
+  avatar: {
+    backgroundColor: red[500],
+  },
+  content: {
+    width: "50%",
+  },
+  field: {
+    display: "flex",
+    justifyContent: "space-between",
+    paddingTop: "10px",
+  },
+}));
 
-  const UserInfo =(props)=> {
-    const classes = useStyles();
-    const [changePasswd, setChangePasswd] = useState(false);
-    const [isAddUser, setIsAddUser] = useState(false);
-    let modalChild = null;
+const UserInfo = (props) => {
+  const classes = useStyles();
+  const [changePasswd, setChangePasswd] = useState(false);
+  const [isAddUser, setIsAddUser] = useState(false);
+  let modalChild = null;
 
-    function cancelModalHandler(){
-      setChangePasswd(false);
-      setIsAddUser(false);
-    }
+  function cancelModalHandler() {
+    setChangePasswd(false);
+    setIsAddUser(false);
+  }
 
-    function changePasswdHandler(){
-      setChangePasswd(true);
-    }
+  function changePasswdHandler() {
+    setChangePasswd(true);
+  }
 
-    function addUserHandler(){
-      setIsAddUser(true);
-    }
+  function addUserHandler() {
+    setIsAddUser(true);
+  }
 
-    if (changePasswd){
-      modalChild = <SetPass modalClosed = {cancelModalHandler}/>
-    }
-    else if (isAddUser){
-      modalChild = <AddUser modalClosed = {cancelModalHandler}/>
-    }
+  if (changePasswd) {
+    modalChild = <SetPass modalClosed={cancelModalHandler} />;
+  } else if (isAddUser) {
+    modalChild = <AddUser modalClosed={cancelModalHandler} />;
+  }
 
-    return(
-          <Aux>
+  return (
+    <Aus>
+      <Modal show={changePasswd || isAddUser} modalClosed={cancelModalHandler}>
+        {modalChild}
+      </Modal>
 
-            <Modal show = {changePasswd || isAddUser} modalClosed = {cancelModalHandler}>
-              {modalChild}
-            </Modal>
+      <Card className={classes.root}>
+        <CardMedia className={classes.media} image={userimage} title="Avatar" />
+        <div>
+          <Typography gutterBottom variant="h5" component="h2">
+            Admin
+          </Typography>
+        </div>
 
-            <Card className={classes.root}>
-              <CardMedia
-              className={classes.media}
-              image={userimage}
-              title="Avatar"
-              />
-              <div>
-                  <Typography gutterBottom variant="h5" component="h2">
-                    Admin
-                  </Typography>
-                </div>
-              
-              <CardContent className={classes.content}>
-                
-                <div className={classes.field}>
-                  <Typography variant="body2" component="h3">
-                    Full name:
-                  </Typography>
-                  <Typography variant="body2" component="h3">
-                    admin
-                  </Typography>
-                </div>
-                <div className={classes.field}>
-                  <Typography variant="body2" component="h3">
-                    Username:
-                  </Typography>
-                  <Typography variant="body2" component="h3">
-                    admin
-                  </Typography>
-                </div>
-                <Button size="small" color="primary" onClick = {changePasswdHandler}>
-                Change Password
-              </Button>
-                <div className={classes.field}>
-                  <Typography variant="body2" component="h3">
-                    Role:
-                  </Typography>
-                  <Typography variant="body2" component="h3">
-                    admin
-                  </Typography>
-                </div>
-              
-              </CardContent>
-              <Button size="small" color="primary" onClick = {addUserHandler}>
-                Add user...
-              </Button>
-              
-          </Card>
-        </Aux>
-        
-    );  
-}
+        <CardContent className={classes.content}>
+          <div className={classes.field}>
+            <Typography variant="body2" component="h3">
+              Full name:
+            </Typography>
+            <Typography variant="body2" component="h3">
+              admin
+            </Typography>
+          </div>
+          <div className={classes.field}>
+            <Typography variant="body2" component="h3">
+              Username:
+            </Typography>
+            <Typography variant="body2" component="h3">
+              admin
+            </Typography>
+          </div>
+          <Button size="small" color="primary" onClick={changePasswdHandler}>
+            Change Password
+          </Button>
+          <div className={classes.field}>
+            <Typography variant="body2" component="h3">
+              Role:
+            </Typography>
+            <Typography variant="body2" component="h3">
+              admin
+            </Typography>
+          </div>
+        </CardContent>
+        <Button size="small" color="primary" onClick={addUserHandler}>
+          Add user...
+        </Button>
+      </Card>
+    </Aus>
+  );
+};
 
 export default UserInfo;
