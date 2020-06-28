e1b456ef7ea1fb2f7ceb8bbfe641bd7d911ea33a
khoavuong <50255410+khoavuong@users.noreply.github.com>
2020-06-28 16:15:53 +0700

    Auto/Manual mode

--- client/src/components/Publisher.js
+++ client/src/components/Publisher.js
@@ -1,53 +1,66 @@
 import React, { useState } from "react";
-import { InputNumber, Button, message, Row, Col } from "antd";
+import { InputNumber, message, Row, Col, Switch, Space } from "antd";
 import styled from "styled-components";
+import { GiSpeaker } from "react-icons/gi";
 
 const FlexBetween = styled.div`
   display: flex;
   align-items: center;
   justify-content: space-between;
-  margin-bottom: 10px;
 `;
 
 export const Publisher = ({ speaker, client }) => {
-  const [devicePower, setDevicePower] = useState(null);
+  const [power, setPower] = useState(0);
+  const [isAuto, setIsAuto] = useState(speaker.auto);
 
-  const onPublishClick = () => {
-    if (devicePower != null) {
+  const publishHandler = () => {
+    if (!isNaN(power)) {
       client.publish(
         "Topic/Speaker",
-        `[{ device_id: ${speaker}, values: ["1", "${devicePower}"] }]`,
+        `[{ "device_id": "${speaker.name}", "values": ["1", "${power}"] }]`,
         () => {
           message.success(
-            `Published power level ${devicePower} to ${speaker} successfully`,
-            3
+            `Published power level ${power} to ${speaker.name} successfully`
           );
         }
       );
-    } else message.error(`Please choose a power level for ${speaker}`, 3);
+    } else
+      message.error(
+        `Please choose a power between 0 and 5000 for ${speaker.name}`
+      );
   };
 
   return (
-    <Row>
-      <Col span={11}>
-        <b style={{ marginRight: "10px" }}>{speaker}: </b>
+    <Row style={{ display: "flex", alignItems: "center" }}>
+      <Col span={10}>
+        <b>{speaker.name}: </b>
       </Col>
-      <Col span={13}>
+      <Col span={14}>
         <FlexBetween>
-          <InputNumber
-            min={0}
-            max={5000}
-            onChange={(value) => {
-              setDevicePower(value);
-            }}
-          />
-          <Button
-            style={{ backgroundColor: "green" }}
-            type="primary"
-            onClick={onPublishClick}
-          >
-            Publish
-          </Button>
+          <Space>
+            <GiSpeaker size={"2em"} />
+            <InputNumber
+              disabled={isAuto}
+              min={0}
+              max={5000}
+              defaultValue={0}
+              onChange={(value) => setPower(value)}
+              onBlur={publishHandler}
+            />
+          </Space>
+
+          <Space>
+            <b>Auto:</b>
+            <Switch
+              defaultChecked={isAuto}
+              onChange={(value) => {
+                message.success(
+                  `Switch ${speaker.name} to ${value ? "auto" : "manual"} mode`
+                );
+                setIsAuto(value);
+              }}
+            />
+          </Space>
         </FlexBetween>
       </Col>
     </Row>
